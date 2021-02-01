6bc1580d108707390c7f0806e7390fdae834ea99
Erik Jung <erik.jung@5pectre.com>
2021-02-01 20:17:00 +0100

    Try 2

---
+++ startseite.php
@@ -0,0 +1,54 @@
+<?php /* Template Name: Startseite */ get_header(); ?>
+	<section id="header">
+		<div class="wrapper">
+			<div class="absolute heading">
+				<h1>Investiere jetzt in Deine Zukunft!</h1>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				<a href="#" class="button">
+					<span>Button</span>
+				</a>
+			</div>
+		</div>
+	</section>
+
+	<section id="about">
+		<div class="bar">
+			<div class="wrapper">
+				<div class="absolute img">
+
+				</div>
+				<div class="absolute about">
+					<h2>Deine Ziele machen uns besonders</h2>
+					<div class="indent">
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+						<a href="#" class="button_dark">
+							<span>Button</span>
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section id="centered">
+		<div class="absolute description">
+			<h2>Und was macht dich so besonders?</h2>
+			<div class="indent">
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			</div>
+		</div>
+	</section>
+
+	<section id="portfolio">
+		<div class="wrapper">
+			<div class="portfolio">
+				<h2>Lass uns gemeinsam etwas erschaffen</h2>
+				<div class="indent">
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				</div>
+			</div>
+		</div>
+		<div class="portfolio-img absolute"></div>
+	</section>
+
+<?php get_footer(); ?>
